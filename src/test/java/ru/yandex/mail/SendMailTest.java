c52e6080ebced26a5b9d8a47a45e8ff8dc3f76c3
monprot <monprot@gmail.com>
2020-11-02 00:24:25 +0300

    some changes

--- src/test/java/ru/yandex/mail/SendMailTest.java
+++ src/test/java/ru/yandex/mail/SendMailTest.java
@@ -44,7 +44,7 @@ public class SendMailTest extends Settings{
          */
         in.clickInputsMail();//нажать на кнопку входящие
         in.clickRefresh();//нажать на кнопку обновить
-        Thread.sleep(1500);
+        Thread.sleep(3500);
         in.assertMail1();
 
         /**
